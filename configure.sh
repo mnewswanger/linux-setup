569107b84f190ea1b80a17f49038ddb2b8d84ba6
Mike Newswanger <mike.newswanger@elastic.co>
2018-05-25 15:03:31 -0400

    Add conditional displays around manually installed applications

--- configure.sh
+++ configure.sh
@@ -16,12 +16,29 @@ ansible-playbook "${DIR}"/setup/configure.yml
 # sudo apt-get install -f
 # run_keybase
 
-"${DIR}"/setup/oh-my-zsh.sh
+if [ ! -d ~/.oh-my-zsh ]; then
+    "${DIR}"/setup/oh-my-zsh.sh
+fi
 
-echo "Pre-Requisite setup complete.  The following needs to be installed manually:"
-echo "  - Slack"
-echo "  - Spotify"
-echo "  - Sublime"
-echo "  - Terraform"
-echo "  - Visual Studio Code"
-echo "  - Zoom"
+echo "Pre-Requisite setup complete.  The following applications to be installed manually:"
+if ! dpkg -l | grep ^ii | grep google-chrome 2>&1 > /dev/null; then
+    echo "  - Chrome (Manual)"
+fi
+if ! snap list | grep slack 2>&1 > /dev/null; then
+    echo "  - Slack (Snap)"
+fi
+if ! snap list | grep spotify 2>&1 > /dev/null; then
+    echo "  - Spotify (Snap)"
+fi
+if ! snap list | grep sublime-text 2>&1 > /dev/null; then
+    echo "  - Sublime Text (Snap)"
+fi
+if ! which terraform 2>&1 > /dev/null; then
+    echo "  - Terraform (Manual)"
+fi
+if ! snap list | grep vscode 2>&1 > /dev/null; then
+    echo "  - Visual Studio Code (Snap)"
+fi
+if ! snap list | grep slack 2>&1 > /dev/null; then
+    echo "  - Zoom (Manual)"
+fi
